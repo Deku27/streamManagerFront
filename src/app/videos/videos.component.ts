f65f86a51863356b27ef206359132d40cd08cd48
Deku27 <ElMehdi.LAHRACH@neo-soft.fr>
2018-07-24 15:16:33 +0200

    " add Edit/Remove Video Feature "

--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -2,53 +2,69 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Video } from '../../models/video';
 import { VideosService } from '../../services/videos.service';
 import { FormGroup, FormControl } from '../../../node_modules/@angular/forms';
-import { Message } from '../../../node_modules/primeng/primeng';
-
+import { Message, ConfirmationService } from '../../../node_modules/primeng/primeng';
 
 @Component({
   selector: 'app-videos',
   templateUrl: './videos.component.html',
   styleUrls: ['./videos.component.css'],
+  providers: [ConfirmationService]
 
   // encapsulation: ViewEncapsulation.None,
 })
 export class VideosComponent implements OnInit {
-
   msgs: Message[] = [];
   videos: Video[];
-  videosTableCols = [
-    {field: 'filename' }, { field : 'audio' }, { field : 'subtitle' }, { field : 'pmt' }, { field : 'color' },
-    { field : 'resolution' }, { field :  'ocs' }, { field : 'csa5' },  { field : 'format' }, { field : 'enabled' },
-    { field : 'description' }, { field : ' status' },
-  ];
-  newVideoForm = new FormGroup ({
-    filename    : new FormControl({value: '', disabled: true}),
-    description : new FormControl({value: '', disabled: false}),
-    color       : new FormControl({value: false , disabled: false}),
-    resolution  : new FormControl({value: '', disabled: false}),
-    ocs         : new FormControl({value: false , disabled: false}),
-    csa5        : new FormControl({value: false , disabled: false}),
-    format      : new FormControl({value: '16/9', disabled: false}),
-    enabled     : new FormControl({value: false, disabled: false}),
-  });
-
+  videosTableCols: any;
+  newVideoForm: any;
   display = false;
   uploded = false;
   uploadError = false;
-  constructor( private videoService: VideosService) { }
 
-  ngOnInit() {
+  dialogState: string;
+  videoID: number;
 
-    this.videoService.getVideos()
-      .subscribe(data => {
-        this.videos = data;
-      }, err => {
-        console.log(err);
-      });
+  constructor(private videoService: VideosService, private confirmationService: ConfirmationService) {
+    this.videosTableCols = [
+      { field: 'filename' },
+      { field: 'audio' },
+      { field: 'subtitle' },
+      { field: 'pmt' },
+      { field: 'color' },
+      { field: 'resolution' },
+      { field: 'ocs' },
+      { field: 'csa5' },
+      { field: 'format' },
+      { field: 'enabled' },
+      { field: 'description' },
+      { field: ' status' }
+    ];
+    this.newVideoForm = new FormGroup({
+      filename: new FormControl({ value: '', disabled: true }),
+      description: new FormControl({ value: '', disabled: false }),
+      color: new FormControl({ value: false, disabled: false }),
+      resolution: new FormControl({ value: '', disabled: false }),
+      ocs: new FormControl({ value: false, disabled: false }),
+      csa5: new FormControl({ value: false, disabled: false }),
+      format: new FormControl({ value: '16/9', disabled: false }),
+      enabled: new FormControl({ value: false, disabled: false })
+    });
   }
 
-  saveVideo() {
+  ngOnInit() {
+    this.videoService.getVideos().subscribe(
+      data => {
+        this.videos = data;
+        console.log(data);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
 
+
+  saveVideo() {
     let filename = this.newVideoForm.getRawValue().filename;
     let description = this.newVideoForm.getRawValue().description;
     let color = this.newVideoForm.getRawValue().color;
@@ -58,63 +74,148 @@ export class VideosComponent implements OnInit {
     let resolution = this.newVideoForm.getRawValue().resolution;
     let enabled = this.newVideoForm.getRawValue().enabled;
 
-    let video: Video = new Video(filename, description, color, resolution, ocs, csa5, format, enabled);
-  this.videoService.saveVideo(video)
-      .subscribe(
+    let video: Video = new Video(
+      filename,
+      description,
+      color,
+      resolution,
+      ocs,
+      csa5,
+      format,
+      enabled
+    );
+    if (this.dialogState === 'create'){
+      this.videoService.saveVideo(video).subscribe(
         data => {
           this.reloadVideoTable();
           this.msgs = [];
-          this.msgs.push({severity: 'success', summary: 'Video successfully added'});
+          this.msgs.push({
+            severity: 'success',
+            summary: 'Video successfully added'
+          });
           console.log(data);
         },
         err => {
-           console.log(err);
+          console.log(err);
         }
       );
-    this.display = false;
+      this.display = false;
+      this.newVideoForm.reset();
+    }else{
+      this.videoService.editVideo(this.videoID, video).subscribe(
+        data => {
+          this.msgs = [];
+          this.msgs.push({
+            severity: 'success',
+            summary: 'Video Edited successfully '
+          });
+          this.reloadVideoTable();
+        },
+        err => {
+          console.log(err);
+        }
+      );
+      this.display = false;
+    }
+
+  }
+
+  getVideoByID() {
+    this.videoService.getVideoByID(this.videoID).subscribe(
+      data => {
+        this.fillEditForm(data);
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
   // ***** Upload functions
-  onSelect(event) {
+  changeFileName(event) {
     let videoToUpload: File = event.files[0];
-    this.newVideoForm.patchValue({filename : videoToUpload.name });
+    this.newVideoForm.patchValue({ filename: videoToUpload.name });
   }
-  onClear() {
-    this.newVideoForm.patchValue({filename : '' });
+
+  clear() {
+    this.newVideoForm.patchValue({ filename: '' });
     this.uploded = false;
     this.uploadError = false;
   }
-  onRemove(event) {
-    this.newVideoForm.patchValue({filename : '' });
+  removeUploadedFile(event) {
+    this.newVideoForm.patchValue({ filename: '' });
     this.uploded = false;
     this.uploadError = false;
   }
 
   uploadVideo(event, uploadForm) {
     let videoToUpload: File = event.files[0];
-    this.videoService.uploadVideo(videoToUpload)
-      .subscribe(
-        data => {
-          this.uploded = true;
-        },
-        err => {
-          this.uploadError = true;
-        }
-      );
-  }
-
-
-  reloadVideoTable() {
-    this.videoService.getVideos()
-    .subscribe(
-        data => {
-            this.videos = data;
-        }, err => {
-            console.log(err);
-        }
+    this.videoService.uploadVideo(videoToUpload).subscribe(
+      data => {
+        this.uploded = true;
+      },
+      err => {
+        this.uploadError = true;
+      }
     );
   }
-  showDialog() {
-    this.display = true;
+
+  ConfirmDeleteVideo(id: number) {
+    console.log(id);
+    this.confirmationService.confirm({
+      message: 'By deleting this Video, you are going to delete every stream related to it, do you want to proceed?',
+      header: 'Delete Confirmation',
+      accept: () => {
+        this.videoService.deleteVideo(id).subscribe(
+          data => {
+            this.reloadVideoTable();
+            this.msgs = [];
+            this.msgs.push({
+              severity: 'success',
+              summary: 'Video successfully deleted'
+            });
+          },
+          err => {
+            console.log(err);
+          }
+        );
+      },
+      reject: () => {
+        //  this.msgs = [{severity:'info', summary:'Rejected', detail:'You have rejected'}];
+      }
+    });
+
+  }
+  showDialog(videoID) {
+    if (videoID === undefined) {
+      this.dialogState = 'create';
+      this.newVideoForm.reset();
+      this.display = true;
+    } else {
+      this.videoID = videoID;
+      this.dialogState = 'edit';
+      this.getVideoByID();
+      this.display = true;
+    }
+  }
+  reloadVideoTable() {
+    this.videoService.getVideos().subscribe(
+      data => {
+        this.videos = data;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+  fillEditForm(data){
+    this.newVideoForm.patchValue({ filename: data.filename });
+    this.newVideoForm.patchValue({ description: data.description });
+    this.newVideoForm.patchValue({ color: data.color });
+    this.newVideoForm.patchValue({ ocs: data.ocs });
+    this.newVideoForm.patchValue({ csa5: data.csa5  });
+    this.newVideoForm.patchValue({ format: data.format });
+    this.newVideoForm.patchValue({ resolution: data.resolution });
+    this.newVideoForm.patchValue({ enabled: data.enabled });
   }
 }
--- src/services/videos.service.ts
+++ src/services/videos.service.ts
@@ -1,52 +1,58 @@
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import {Injectable} from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Video } from '../models/video';
 import { Observable } from '../../node_modules/rxjs';
 import { map, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class VideosService {
-    constructor ( public http: HttpClient) {
-    }
-    getVideos() {
-        return this.http.get('http://localhost:8080/videos')
-            .pipe(map(res => <Video[]> res));
-
-    }
-
-    getVideosInfo() {
-      return this.http.get('http://localhost:8080/getVideosInfo')
+  constructor(public http: HttpClient) {}
+  getVideos() {
+    return this.http
+      .get('http://localhost:8080/videos')
+      .pipe(map(res => <Video[]>res));
+  }
+  getVideosInfo() {
+    return this.http
+      .get('http://localhost:8080/getVideosInfo')
       .pipe(map(res => res));
-    }
+  }
 
+  getVideoByID(id: number) {
+    return this.http
+      .get('http://localhost:8080/video/' + id)
+      .pipe(map(res => <Video>res));
+  }
 
-    saveVideo(video: Video) {
-      return this.http.post('http://localhost:8080/videos', video)
-          .pipe(map(res => res));
-
-    }
-
-    deleteVideo(id: number) {
-      return this.http.delete('http://localhost:8080/video/' + id)
+  saveVideo(video: Video) {
+    return this.http
+      .post('http://localhost:8080/videos', video)
       .pipe(map(res => res));
-    }
+  }
 
+  editVideo(id: number, video: Video) {
+    return this.http
+      .put('http://localhost:8080/video/' + id, video)
+      .pipe(map(res => res));
+  }
 
-    uploadVideo(fileToUpload: File): Observable<string> {
-      const endpoint = 'http://localhost:8080/uploadVideo';
-      const formData: FormData = new FormData();
-      formData.append('file', fileToUpload, fileToUpload.name);
-      return this.http
-        .post(endpoint, formData, {reportProgress: true, responseType: 'text'}).
-          pipe(
-              map(res => res),
-              catchError(
-                  (err: HttpErrorResponse) => {
-                    return Observable.throw(err.statusText);
-                 }
-              )
-          );
-    }
+  deleteVideo(id: number) {
+    return this.http
+      .delete('http://localhost:8080/video/' + id)
+      .pipe(map(res => res));
+  }
 
-
- }
+  uploadVideo(fileToUpload: File): Observable<string> {
+    const endpoint = 'http://localhost:8080/uploadVideo';
+    const formData: FormData = new FormData();
+    formData.append('file', fileToUpload, fileToUpload.name);
+    return this.http
+      .post(endpoint, formData, { reportProgress: true, responseType: 'text' })
+      .pipe(
+        map(res => res),
+        catchError((err: HttpErrorResponse) => {
+          return Observable.throw(err.statusText);
+        })
+      );
+  }
+}
